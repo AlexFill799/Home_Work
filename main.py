16b3d6e3245ce66f3f38054edb835cfc56ee1ef4
AlexFill799 <59891675+AlexFill799@users.noreply.github.com>
2020-01-15 20:15:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
+# Целые числа
 a = 7
-b = 90
+b = 23
+print ('Целые числа')
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
 
+# Дробные числа
+a = (float(1.73))
+b = (float(7.75))
+print ('теперь float')
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
 
+# Строки
+a = ('Hello ')
+b = ('World')
+print ('Теперь строки')
+print (a + b)
+
+# Теперь input
+print ('Введите целое число')
+a = (input(int))
+print ('Введите целое число №2')
+b = (input(int))
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
