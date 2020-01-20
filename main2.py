c2fc38b074f062d6591f39bf58225360897056a7
AlexFill799 <59891675+AlexFill799@users.noreply.github.com>
2020-01-20 16:17:07 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -4,11 +4,14 @@ i.append (int (input ('введите первое число:')))
 i.append (int (input ('введите второе число')))
 i.append (int (input ('введите третье число')))
 print (i)
-f = (sum(i))
+print (type (i))
+f = (0)
+for e in i:
+	f = f + e
+print (f)
 if f > 100:
 	print ('в яблочко')
 elif f < 100:
 	print ('не в яблочко')
 else:
 	print ('ну капец')		
-print (f)
